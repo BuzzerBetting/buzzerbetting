52e753c87beee21552ffe8cd20bb307c5cdaa598
BuzzerBetting <jordanboyd2002.0@gmail.com>
2026-06-29 17:37:08 +0100

    Add OC FGS Find function with CORS support

---
+++ netlify/functions/oc-fgs-find.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json'
+};
+
+exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') return { statusCode: 204, headers: CORS, body: '' };
+
+  const { home, away } = event.queryStringParameters || {};
+  if (!home || !away) return {
+    statusCode: 400, headers: CORS,
+    body: JSON.stringify({ ok: false, error: 'home and away required' })
+  };
+
+  return new Promise((resolve) => {
+    const url = `http://178.128.40.248:3000/api/oc-fgs-find?home=${encodeURIComponent(home)}&away=${encodeURIComponent(away)}`;
+    http.get(url, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve({
+        statusCode: 200, headers: CORS, body: data
+      }));
+    }).on('error', (err) => resolve({
+      statusCode: 200, headers: CORS,
+      body: JSON.stringify({ ok: false, error: err.message })
+    }));
+  });
+};
